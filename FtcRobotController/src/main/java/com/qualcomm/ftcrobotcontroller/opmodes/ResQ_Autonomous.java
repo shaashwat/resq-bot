ac0c29caa96202314a99d8b6833426527cddc6ad
dakeenekid <dbkeene.tsyc@gmail.com>
2015-10-15 15:01:13 -0400

    Autonomous Code Created

---
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/ResQ_Autonomous.java
@@ -0,0 +1,24 @@
+package com.qualcomm.ftcrobotcontroller.opmodes;
+
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+/**
+ * Created by Aman on 10/11/2015.
+ */
+public class ResQ_Autonomous extends ResQ_Library {
+
+    @Override
+    public void init() {
+
+    }
+
+    @Override
+    public void loop() {
+
+    }
+
+    @Override
+    public void stop() {
+
+    }
+}
